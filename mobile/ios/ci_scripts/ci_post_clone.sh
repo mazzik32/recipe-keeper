e03e73b8e7adfabab5a2b32855806aed61b6af2c
mazzik32 <45147697+mazzik32@users.noreply.github.com>
2026-03-08 18:47:23 +0100

    ci: Add Xcode Cloud post-clone script to install Node.js, CocoaPods, npm packages, and iOS pods.

---
+++ mobile/ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+echo "=> Installing Node.js and CocoaPods..."
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew update
+brew install node
+brew install cocoapods
+
+# This script is executed from mobile/ios/ci_scripts/
+# We need to navigate up to the `mobile` folder to install npm packages
+echo "=> Navigating to the mobile project root..."
+cd ../..
+
+echo "=> Installing JS dependencies..."
+npm install
+
+echo "=> Navigating to ios directory and installing Pods..."
+cd ios
+pod install
+
+echo "=> Done Xcode Cloud setup!"
